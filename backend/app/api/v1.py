c146025e3e1149e1e553345d64b6385be04d5c00
Subhasis Roy <roysubhasis32@gmail.com>
2026-07-21 23:35:28 +0530

    Update v1.py

--- backend/app/api/v1.py
+++ backend/app/api/v1.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.routes import health
+from app.auth import routes as auth_routes
 
 api_router = APIRouter()
 
 # Include versioned routers
 api_router.include_router(health.router, tags=["System Health"])
+api_router.include_router(auth_routes.router, tags=["Clerk Authentication"])
