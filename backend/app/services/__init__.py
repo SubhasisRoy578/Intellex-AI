072a11f5de5ecca3803c9762887bf8aeeed7f86a
Subhasis Roy <roysubhasis32@gmail.com>
2026-07-22 23:32:22 +0530

    Update __init__.py

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,3 +1,9 @@
 from app.services.db_service import DBBaseService
+from app.services.ai_service import AIService
+from app.services.chat_service import ChatService
 
-__all__ = ["DBBaseService"]
+__all__ = [
+    "DBBaseService",
+    "AIService",
+    "ChatService"
+]
